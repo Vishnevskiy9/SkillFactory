5433bfb607b8fb4227d401112cc31d6cc14edd0b
AnnaFedoseeva <93124087+Vishnevskiy9@users.noreply.github.com>
2025-08-19 11:19:35 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+
+	 string myName;
+	 myName = "Jane";
+
+	 Console.WriteLine(myName);
+	 
+	 Console.ReadKey();
+  }
+}
